8c4622ab8cad785be8425aeaba0c2337072df7f2
Michael Aliendro <mgbaliendro@gmail.com>
2021-11-25 19:24:03 -0300

    Start: basic app setup
    Initial settings for eslint, typescript, postcss, tailwindcss and git.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    flex: {
+      pancake: '0 1 250px',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
