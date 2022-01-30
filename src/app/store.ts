9e2c8821374ccbb5ce47a5ad118cbcad85a7b027
aliendro <mgbaliendro@gmail.com>
2022-01-29 23:00:42 -0300

    Refactor: code cleanup

--- src/app/store.ts
+++ src/app/store.ts
@@ -15,9 +15,7 @@ const store = configureStore({
 
 setupListeners(store.dispatch);
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = () => useDispatch<typeof store.dispatch>();
+export const useAppSelector: TypedUseSelectorHook<ReturnType<typeof store.getState>> = useSelector;
 
 export default store;
